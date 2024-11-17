c58e9cb3d63d7fb45051228ec7b3455de456153c
Glib116 <glebtomas99@gmail.com>
2024-11-17 12:59:55 +0200

    Add files via upload

---
+++ memo_qss.py
@@ -0,0 +1,39 @@
+style_sheet = """   
+                  QApplication {
+                      background: blue;
+                      color: yellow;
+                  }
+                  QWidget {
+                      background: gray;
+                      color: white;
+                  }
+                  QPushButton {
+                      background: black;
+                      border: 3px solid black;
+                      border-radius: 7px;
+                      font-family: Comic Sans, sans-serif;
+                      font-size: 20px;
+                      font-weight: bold;
+                  }
+                  QGroupBox {
+                      background: bisque;
+                      border: 3px solid black;
+                      border-radius: 7px;
+                  }
+                  QRadioButton {
+                      background: pink;
+                      border: 3px solid black;
+                      border-radius: 7px;
+                      font-family: Comic Sans, sans-serif;
+                      font-size: 25px;
+                      font-weight: bold;
+                  }
+                  QLabel {
+                      background: darkblue;
+                      color: white;
+                      font-family: Comic Sans, sans-serif;
+                      font-size: 25px;
+                      border: 1px solid black;
+                      border-radius: 7px;
+                  }
+                  """
